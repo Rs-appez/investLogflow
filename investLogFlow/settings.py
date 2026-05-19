996ad319f0ab28c5bf4b555395f58a54a02d9007
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-19 12:20:01 +0200

    refactor(settings): rename API_KEY to MASSIVE_API_KEY
    Renamed the environment variable API_KEY to MASSIVE_API_KEY in settings.py
    for clarity and to avoid ambiguity. Update your .env files accordingly.

--- investLogFlow/settings.py
+++ investLogFlow/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(env_file)
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 SECRET_KEY = env("SECRET_KEY")
-API_KEY = env("API_KEY")
+MASSIVE_API_KEY = env("MASSIVE_API_KEY")
 
 DEBUG = env.bool("DEBUG", default=False)
 
