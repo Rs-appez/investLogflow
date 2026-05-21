e14b1f7ddb2ea16c84f4a157efe3bc85879bd680
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-21 16:59:34 +0200

    feat(tracker): add paginated stocks list view and template
    Added a new paginated stocks list view with HTMX support in the tracker
    app. Created partial and full templates for displaying stocks, and
    updated the navbar to include a link to the stocks page. Registered the
    new route in the tracker app's URLs.

--- apps/tracker/views.py
+++ apps/tracker/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
+from .models import Stock
 
 tracker_app = "tracker"
+tracker_partials = f"{tracker_app}/partials"
 
 
 @login_required
 def home(request):
     return render(request, f"{tracker_app}/home.html")
+
+
+@login_required
+def all_stocks(request):
+    pagesize = 20
+    page = request.GET.get("page", 1)
+    paginator = Paginator(Stock.objects.all(), pagesize)
+    stocks = paginator.get_page(page)
+    template = (
+        f"{tracker_partials}/stocks_table.html"
+        if request.headers.get("HX-Request")
+        else f"{tracker_app}/stocks.html"
+    )
+    return render(request, template, {"stocks": stocks})
