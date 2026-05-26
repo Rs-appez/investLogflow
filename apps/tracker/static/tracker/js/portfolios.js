82e23fb07c091636fa46b9f93d42f7a9eacf9559
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-26 12:04:22 +0200

    feat(tracker): add JS to open add portfolio modal
    Introduce a JavaScript file that attaches a click event listener to the
    "Add Portfolio" button. When clicked, it displays the add portfolio modal
    dialog. This enhances the user experience by enabling modal-based portfolio
    creation.

---
+++ apps/tracker/static/tracker/js/portfolios.js
@@ -0,0 +1,6 @@
+add_portfolio_btn = document.getElementById("add-portfolio-btn");
+add_portfolio_modal = document.getElementById("add-portfolio-modal");
+
+add_portfolio_btn.addEventListener("click", function () {
+  add_portfolio_modal.showModal();
+});
